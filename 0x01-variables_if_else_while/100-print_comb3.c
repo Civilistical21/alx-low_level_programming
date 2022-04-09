08a9fb5790eca6a0c117ea45c6d8389e354e10be
Civilistical21 <jamescephason@gmail.com>
2022-04-08 17:50:30 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,9 +7,9 @@
 int main(void)
 {
 int c, i;
-for (c = '0' c <= '9' c++)
+for (c = '0'; c <= '9'; c++)
 {
-for (i = '0' i <= '9' i++)
+for (i = '0'; i <= '9'; i++)
 {
 if (c < i)
 {
@@ -18,7 +18,7 @@ putchar(i);
 if (c != '8' || (c == '8' && i != '9'))
 {
 putchar(',');
-putchar(',');
+putchar(' ');
 }
 }
 }
