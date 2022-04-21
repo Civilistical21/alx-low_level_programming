b5a459389bdb6628281082d22dc1ffe1bd1638d8
Civilistical21 <jamescephason@gmail.com>
2022-04-21 10:52:58 -0700

    Task 3 fix

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * _puts - print a string
- * Ã@str: pointer char
+ * @str: pointer char
  * return: void
  */
 void _puts(char *str)
@@ -10,7 +10,7 @@ void _puts(char *str)
 int i;
 for (i = 0; str[i] != '\0'; i++)
 {
-_putchar(strâœ[i]);
+_putchar(str[i]);
 }
 _putchar('\n');
 return;
