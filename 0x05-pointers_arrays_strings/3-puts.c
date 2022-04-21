28f4dd390db691a77c15be9b02b90fa3764c08a3
Civilistical21 <jamescephason@gmail.com>
2022-04-21 15:54:37 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,35 +1,16 @@
 #include "holberton.h"
-
-
-
 /**
-
  * _puts - print a string
-
  * @str:pointer char
-
  * return:void
-
  */
-
 void _puts(char *str)
-
 {
-
 	int i;
-
-
-
 	for (i = 0; str[i] != '\0'; i++)
-
 	{
-
 		putchar(str[i]);
-
 	}
-
 putchar('\n');
-
 return;
-
 }
