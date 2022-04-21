827027487c61c454993b2162dea8b857d8cda60a
Civilistical21 <jamescephason@gmail.com>
2022-04-21 16:29:20 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,10 @@
 #include "holberton.h"
-
-
-
 /**
-
  * print_rev - print a string
-
  * @s:pointer char
-
  * return:void
-
  */
-
 void print_rev(char *s)
-
 {
 
 int i, n;
@@ -37,7 +28,6 @@ putchar(s[i]);
 }
 
 putchar('\n');
-
 return;
 
 }
