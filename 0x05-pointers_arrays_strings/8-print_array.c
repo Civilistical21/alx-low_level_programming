f50a914fab1c589fb44e030748a04a88fa6cc707
Civilistical21 <jamescephason@gmail.com>
2022-04-21 17:05:23 -0700

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,44 +1,21 @@
 #include "holberton.h"
-
 #include <stdio.h>
-
-
-
 /**
-
  * print_array - display array
-
  * @a:integer
-
  * @n:integer
-
  *Return:void
-
  */
-
 void print_array(int *a, int n)
-
 {
-
 int i;
-
 for (i = 0; i < n; i++)
-
-	{
-
-	printf("%d", a[i]);
-
-		if (i < (n - 1))
-
-		{
-
-		printf(", ");
-
-		}
-
-	}
-
-	printf("\n");
-
+{
+printf("%d", a[i]);
+if (i < (n - 1))
+{
+printf(", ");
 }
-clear
+}
+printf("\n");
+}
