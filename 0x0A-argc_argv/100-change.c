55609889d85c64da5c36ec37fa141c43858b6f16
Civilistical21 <jamescephason@gmail.com>
2022-05-03 15:42:40 -0700

    Task 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,46 @@
+#include  <stdio.h>
+#include <stlib.h>
+/**
+ * main - prints the  minimum number  of coins to make change for a given amount
+ * @argc: argument  count
+ * @argv: array of pointers to argument strings
+ * Return: number of  coins or 1
+ */
+int main(int argc, char *argv[])
+{
+	int   amount,  coins;
+	if (argc !=  2)
+	{
+		printf("Error\n");
+		return  (1)
+	}
+	amount = atoi(argv[1]);
+	coins = 0;
+	if (amount > 30)
+	{
+		while (amount  >=  30)
+			amount -= 30, coins++;
+	}
+	if  (amount > 15  && amount < 30)
+	{
+		while  (amount >= 15)
+			amount -=  15, coins++;
+	}
+	if  (amount  > 10 && amount < 15)
+	{
+	        while  (amount >= 10)
+	                amount -= 10, coins++;
+	}
+	if (amount > 2 && amount < 10)
+	{
+		while (amount >= 2)
+			amount -= 2, coins++;
+	}
+	if (amount == 1 ||  amount == 2 || amount == 10 || amount 15  || amount == 30)
+	{
+		coins++;
+	}
+	printf("%d\n", coins);
+	return  (0);
+}
+
