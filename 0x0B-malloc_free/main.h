d0038907118bdf25efe8607f203ef3b2e55db368
Civilistical21 <jamescephason@gmail.com>
2022-05-05 15:30:15 -0700

    Task header file

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #definr MAIN_H
-
+#include <stdio.h>
+#include <stlib.h>
+int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
@@ -8,6 +10,4 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
-_putchar;
-
 #endif
