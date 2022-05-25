c04922853be24eec60d822fda475226c56b0b2d7
Civilistical21 <jamescephason@gmail.com>
2022-05-24 18:43:35 -0700

    commit 0

---
+++ 0x12-singly_linked_lists/3-add_node-end.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+/**
+ * add_node_end - Add a new node at the end of a list.
+ * @head: Address of the first node of a list.
+ * @str: Address of the string to insert into the new node.
+ * Return: Address of the new node.
+ **/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *temp, *temp2;
+	unsigned int length = 0;
+
+#include "lists.h"
+/**
+ * add_node_end - Add a new node at the end of a list.
+ * @head: Address of the first node of a list.
+ * @str: Address of the string to insert into the new node.
+ * Return: Address of the new node.
+ **/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *temp, *temp2;
+	unsigned int length = 0;
+
+temp->str = strdup(str);
+	if (temp->str == NULL)
+	{
+		free(temp);
+		return (NULL);
+	}
+	while (str[length])
+		length++;
+	temp->len = length;
+	temp->next = NULL;
+
+if (*head == NULL)
+	{
+		*head = temp;
+		return (temp);
+	}
+
+temp2 = *head;
+	while (temp2->next)
+		temp2 = temp2->next;
+	temp2->next = temp;
+	return (temp);
+}
+
