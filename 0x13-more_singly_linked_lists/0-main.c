802bdcfb15f8da208aa5083b657974a1aa94c1bd
Civilistical21 <jamescephason@gmail.com>
2022-05-26 07:21:26 -0700

    head file

---
+++ 0x13-more_singly_linked_lists/0-main.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+head = &hello;
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    new->n = 9;
+    new->next = head;
+    head = new;
+    n = print_listint(head);
+    printf("-> %lu elements\n", n);
+    free(new);
+    return (0);
+}
