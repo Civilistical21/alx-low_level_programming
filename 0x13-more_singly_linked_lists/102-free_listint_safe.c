72afda468c19f11b7d816abc746d3f7cbd3ed335
Civilistical21 <jamescephason@gmail.com>
2022-05-26 15:57:58 -0700

    Task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,3 +1,4 @@
+lines (34 sloc)  507 Bytes
 #include "lists.h"
 #include <stdio.h>
 /**
@@ -8,27 +9,31 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-size_t len = 0;
-int diff;
-listint_t *temp;
-if (!h || !*h)
-return (0);
-while (*h)
-{
-diff = *h - (*h)->next;
-if (diff > 0)
-{
-temp = (*h)->next;
-*h = temp;
-len++;
-}
-else
-{
-*h = NULL;
-len++;
-break;
-}
-}
-*h = NULL;
-return (len);
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			temp = (*h)->next;
+			*h = temp;
+			len++;
+		}
+		else
+		{
+			*h = NULL;
+			len++;
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (len);
 }
