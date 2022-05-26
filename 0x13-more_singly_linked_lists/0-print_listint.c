122119e29d5e7c1634676495d0ddbfb6af62b9df
Civilistical21 <jamescephason@gmail.com>
2022-05-26 07:38:55 -0700

    Task 0 Mandatory corection

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,15 +9,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *tp;
-	unsigned int cntr = 0;
-
-	tp = h;
-	while (tp)
-	{
-		printf("%d\n", tp->n);
-		cntr++;
-		tp = tp->next;
-	}
-	return (cntr);
+const listint_t *tp;
+unsigned int cntr = 0;
+tp = h;
+while (tp)
+{
+printf("%d\n", tp->n);
+cntr++;
+tp = tp->next;
+}
+return (cntr);
 }
