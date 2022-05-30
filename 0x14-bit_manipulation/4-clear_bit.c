30c1e47b8460292a1cdfd4e3803e2f61ba7aed4c
Civilistical21 <jamescephason@gmail.com>
2022-05-30 15:01:00 -0700

    Task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -5,6 +5,7 @@
  * @n: A pointer to the bit.
  * @index: The index to set the value at - indices start at 0.
  *
+
  * Return: If an error occurs - -1.
  *         Otherwise - 1.
  */
@@ -14,4 +15,5 @@ int clear_bit(unsigned long int *n, unsigned int index)
 if (index >= (sizeof(unsigned long int) * 8))
 return (-1);
 *n &= ~(1 << index);
+return (1);
 }
