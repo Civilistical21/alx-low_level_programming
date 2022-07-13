61a1f9de6020585959ef07d827385735989eb1bd
Civilistical21 <jamescephason@gmail.com>
2022-07-13 10:29:56 -0700

    Commit fix

---
+++ 0x18-dynamic_libraries/cep.c
@@ -0,0 +1,28 @@
+#include "python.h"
+#include <stdio.h>
+
+int a, b;
+int add(int a, int b)
+{
+	return a+b;
+}
+
+int div(int a, int b)
+{
+        return a/b;
+}
+
+int mul(int a, int b)
+{
+        return a*b;
+}
+
+int sub(int a, int b)
+{
+        return a-b;
+}
+
+int mod(int a, int b)
+{
+        return a%b;
+}
