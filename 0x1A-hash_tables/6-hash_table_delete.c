fc6d8cef8cf49e5dd87770c1ec747bd99f0177db
Civilistical22 <jamescephason@gmail.comgit config --global user.name Civilistical21git config --global user.email jamescephason@gmail.com>
2022-08-04 13:19:03 -0700

    commit all

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -35,37 +35,3 @@ void free_list(hash_node_t *head)
 	}
 
 }
-
-
-
-/**
-
- * hash_table_delete - deletes a hash table
-
- * @ht: hash table to be deleted
-
- */
-
-void hash_table_delete(hash_table_t *ht)
-
-{
-
-	unsigned long int i;
-
-
-
-	if (!ht)
-
-		return;
-
-
-
-	for (i = 0; i < ht->size; i++)
-
-		free_list(ht->array[i]);
-
-	free(ht->array);
-
-	free(ht);
-
-}
